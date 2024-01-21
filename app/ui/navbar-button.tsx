7da89c1fc41e263daeff7611f6d1f9c726fb1d8f
ilyaKryazh <ilya.kryazik10@gmail.com>
2024-01-21 23:25:14 +0300

    create sidebar

--- app/ui/navbar-button.tsx
+++ app/ui/navbar-button.tsx
@@ -1,8 +1,8 @@
-export default function NavbarButton({ switchToogle, toogle }: { switchToogle: () => void, toogle: boolean }) {
-  //TODO: Toogle navbar section with menu
+export default function NavbarButton({ toogleButton }: { toogleButton: () => void }) {
+
   return (
     <div>
-      <button className="bg-none hover:bg-gray-200 hover:rounded-full cursor-pointer  p-[10px] flex flex-col items-center">
+      <button onClick={toogleButton} className="bg-none hover:bg-gray-200 hover:rounded-full cursor-pointer  p-[10px] flex flex-col items-center">
         <div className="w-[22px] h-[3px] bg-gray-900 mt-[0px]"></div>
         <div className="w-[22px] h-[3px] bg-gray-900 mt-[5px]"></div>
         <div className="w-[22px] h-[3px] bg-gray-900 mt-[5px]"></div>
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react"
 import NavbarButton from "./navbar-button"
 import NavbarSearch from "./navbar-search"
+import '@/app/ui/css/sidebar.css'
 
 export default function Navbar() {
   const [toogle, setToogle] = useState(false)
@@ -13,8 +14,28 @@ export default function Navbar() {
 
   return (
     <div className="flex flex-row gap-3 m-3">
-      <NavbarButton switchToogle={switchToogle} toogle={toogle} />
+      <NavbarButton toogleButton={switchToogle} />
       <NavbarSearch />
+
+      <div className={`${(toogle) ? 'showSideNav' : 'hideSideNav'} absolute top-0 left-0 w-full h-full `}>
+        <div
+          className="z-11 px-10 py-10 w-[35rem] h-full bg-white"
+        >
+          <ul className="text-black flex flex-col justify-between min-h-[250px]">
+            <li className="my-8 uppercase">
+              <a href="/about">About</a>
+            </li>
+            <li className="my-8 uppercase">
+              <a href="/portfolio">Portfolio</a>
+            </li>
+            <li className="my-8 uppercase">
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div onClick={() => setToogle(false)} className="bg-black opacity-30 h-full w-full z-7"></div>
+      </div>
     </div>
+
   )
 }
