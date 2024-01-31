f9ac4f034904706173f7442284a78f870cb54c92
ilyaKryazh <ilya.kryazik10@gmail.com>
2024-01-31 13:06:37 +0300

    update animation

--- app/ui/imageButton.tsx
+++ app/ui/imageButton.tsx
@@ -1,9 +1,22 @@
+'use client'
 import Image from "next/image";
+import { useRef } from "react";
+
+import '@/app/ui/css/buttonAnimation.css'
 
 export default function ImageButton({ url, width, height }: { url: string, width: number, height: number }) {
+  const buttonRef = useRef<HTMLButtonElement>(null)
+
+  function animate() {
+    buttonRef.current?.classList.add('clicked')
+    setTimeout(() => buttonRef.current?.classList.remove('clicked'), 300)
+  }
+
   return (
-    <div className="flex items-center justify-center p-2 rounded w-full">
-      <Image src={url} alt="image" width={width} height={height} />
+    <div className="flex justify-center">
+      <button ref={buttonRef} onClick={animate} className="button flex items-center justify-center p-2 rounded w-fit click:animate-ping" >
+        <Image src={url} alt="image" width={width} height={height} />
+      </button>
     </div>
   )
 }
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -17,9 +17,9 @@ export default function Navbar() {
       <Hamburger toogleButton={switchToogle} />
       <Search />
 
-      <div className={`${(toogle) ? 'showSideNav' : 'hideSideNav'} absolute top-0 left-0 w-full h-full `}>
+      <div className={`sidebar ${(toogle) ? 'open' : 'close'}`}>
         <div
-          className="z-11 px-10 py-10 w-[35rem] h-full bg-white"
+          className={`sidebar-menu ${(toogle) ? 'open' : 'close'} z-11 px-10 py-10 h-full bg-white`}
         >
           <ul className="text-black flex flex-col justify-between min-h-[250px]">
             <li className="my-8 uppercase">
